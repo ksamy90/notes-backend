c6d1d6ad271c9312f893d149ac8da1402fc34d79
samson <samson.omondi1@ibm.com>
2023-04-20 13:42:56 +0300

    add env variables

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 let notes = [
   {
@@ -85,6 +85,6 @@ const unknownEndpoint = (request, response, next) => {
 };
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
