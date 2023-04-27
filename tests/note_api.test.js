5028040e1471c5160e6e4791bcc8b3ee1dc75f4b
samson <samson.omondi1@ibm.com>
2023-04-27 08:40:31 +0300

    initialize database before tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,6 +3,26 @@ const supertest = require("supertest");
 const app = require("../app");
 
 const api = supertest(app);
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+}, 50000);
 
 test("notes are returned as json", async () => {
   await api
@@ -11,14 +31,15 @@ test("notes are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 }, 50000);
 
-test("there are two notes", async () => {
+test("all notes are returned", async () => {
   const response = await api.get("/api/notes");
-  expect(response.body).toHaveLength(3);
+  expect(response.body).toHaveLength(initialNotes.length);
 });
 
-test("the first note is about HTTP methods", async () => {
+test("a specific note is within the returned notes", async () => {
   const response = await api.get("/api/notes");
-  expect(response.body[1].content).toBe("CSS is hard");
+  const contents = response.body.map((r) => r.content);
+  expect(contents).toContain("Browser can execute only JavaScript");
 });
 
 afterAll(async () => {
