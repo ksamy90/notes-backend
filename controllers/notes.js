9ff6930ca0ad39e04237e943a18461c6747d7f6b
samson <samson.omondi1@ibm.com>
2023-04-28 22:41:35 +0300

    add routes for notes and user association

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,9 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const User = require("../models/user");
 
 notesRouter.get("/", async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -17,13 +18,17 @@ notesRouter.get("/:id", async (request, response) => {
 
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
+
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user.id,
   });
-
   const savedNote = await note.save();
-  response.status(201).json(savedNote);
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+  response.json(savedNote);
 });
 
 notesRouter.delete("/:id", async (request, response) => {
--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ usersRouter.post("/", async (request, response) => {
 });
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("notes");
   response.json(users);
 });
 
